523479ab17ac5e619834b3fae8df72ed1c7524ef
Ashwinumbarkar20 <Ashwinumbarkar20@gmail.com>
2023-01-30 11:06:01 +0530

    Code updated

--- index.js
+++ index.js
@@ -15,19 +15,23 @@ function clock()
     document.getElementById("hours").innerText=h;
     document.getElementById("mins").innerText=m;
     document.getElementById("secs").innerText=s;
-    if(ampm="AM")
+
+    if(ampm=="AM")
     {
-        document.getElementById("ampm").style.backgroundImage=("/morning.jpg")
+        //document.getElementById("ampm").style.backgroundImage=("/morning.jpg")
+        document.querySelector("body").style.backgroundImage="url('/morning.jpg')"
+        document.querySelector("h3").style.color="#4C0033"
         let sapns=document.querySelectorAll(".hours, .mins, .secs, .ampm");
         let textspns=document.querySelectorAll(".text")
-        for(s of spans){
-            s.style.backgroundcolor="#C75643"
-            s.style.color="#D08752"
+        for(s of sapns){
+            s.style.backgroundColor ="#CFE8A9"
+            s.style.color="#C21010"
         }
 
         for(s of textspns){
-            s.style.backgroundcolor="#D08752"
-            s.style.color="#FFD4B2"
+            s.style.backgroundColor ="#E64848"
+            s.style.color="#F0EABE"
+            
         }
 
     }
